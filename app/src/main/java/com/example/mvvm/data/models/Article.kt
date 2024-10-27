0faac558fedbe788221b9913983d1284ca531fa3
Kiran <kiran@macappstudio.com>
2024-10-27 09:38:22 +0530

    Added pagination logic for BreakingNewsFragment and SearchNewsFragment.

---
+++ app/src/main/java/com/example/mvvm/data/models/Article.kt
@@ -0,0 +1,22 @@
+package com.example.mvvm.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity
+    (
+    tableName = "Articles"
+)
+data class Article(
+    @PrimaryKey
+    val id: Int? = null,
+    val author: String?,
+    val content: String?,
+    val description: String?,
+    val publishedAt: String?,
+    val source: Source?,
+    val title: String?,
+    val url: String?,
+    val urlToImage: String?
+) : Serializable
